1c744b94417f4e74daccee051798db6185fc40ab
PiNKVOiD <pinkvoid@protonmail.com>
2018-01-20 18:25:41 +0100

    installer

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 
-apt install libmysqld-dev
-pip install dotmap mysqlclient secp256k1
+
+apt install -y python-pip python-dev build-essential libssl-dev libffi-dev libmysqld-dev
+pip install setuptools dotmap mysqlclient secp256k1 base58 mysqlclient
